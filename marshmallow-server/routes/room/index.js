1ec2f41fd20949ea23933bbbf3febe762e6df132
jaeeunahn <jaeeunno1@naver.com>
2020-11-22 03:37:25 +0900

    [ADD] room/create & room/join fin

--- marshmallow-server/routes/room/index.js
+++ marshmallow-server/routes/room/index.js
@@ -22,9 +22,9 @@ router.post('/create', async (req, res) => {
 
     //4. salt 생성
     const salt = crypto.randomBytes(64).toString('base64');
-    //5. 2차 세미나때 배웠던 pbkdf2 방식으로 (비밀번호 + salt) => 암호화된 password
+    
     const hashedCode = crypto.pbkdf2Sync(title, salt, 10000, 64, 'sha512').toString('base64');
-    //6. User email, 암호화된 password, salt, userName 생성!
+    
     const room = await Room.create({
       title: title,
       limitTime: limitTime,
@@ -32,7 +32,6 @@ router.post('/create', async (req, res) => {
       code: hashedCode,
       salt: salt,
     });
-    //7. status: 200 message: SING_UP_SUCCESS, data: id, email, userName 반환! (비밀번호, salt 반환 금지!!)
     console.log(user);
     return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.ROOM_CREATE_SUCCESS, { title: room.title, limitTime: room.limitTime, startTime: room.startTime, code: room.code }));
   } catch(error) {
@@ -43,42 +42,28 @@ router.post('/create', async (req, res) => {
 
 // 방 코드로 들어가기
 router.post('/join', async (req, res) => {
-  //1. req.body에서 데이터 가져오기
-  const { email, password, userName } = req.body;
-  //2. request data 확인하기, email, password, userName data가 없다면 NullValue 반환
-  if (!email || !password || !userName ) {
+
+  const { code } = req.body;
+  if (!code ) {
     console.log("필요한 값이 없습니다");
-    return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+    return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, "방 코드가 제대로 되지 않았음"));
   }
   try {
-    //3. 존재하는 이메일인지 확인하기. 이미 존재하는 이메일면 ALREADY ID 반환
-    const alreadyEmail = await User.findOne({
+    const alreadyCode = await Room.findOne({
       where: {
-        email: email,
-      }
+        code: code,
+      },
+      attributes: ['title', 'limitTime', 'startTime']
     });
 
-    if (alreadyEmail) {
-      console.log('이미 존재하는 이메일 입니다.')
-      return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.ALREADY_ID));
+    if (!alreadyCode) {
+      console.log('존재하지 않는 코드입니다.');
+      return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NO_ROOM));
     }
-    //4. salt 생성
-    const salt = crypto.randomBytes(64).toString('base64');
-    //5. 2차 세미나때 배웠던 pbkdf2 방식으로 (비밀번호 + salt) => 암호화된 password
-    const hashedPassword = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('base64');
-    //6. User email, 암호화된 password, salt, userName 생성!
-    const user = await User.create({
-      email: email,
-      password: hashedPassword,
-      userName: userName,
-      salt: salt,
-    });
-    //7. status: 200 message: SING_UP_SUCCESS, data: id, email, userName 반환! (비밀번호, salt 반환 금지!!)
-    console.log(user);
-    return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.SIGN_UP_SUCCESS, { id: user.id, email, userName }));
+    return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.ROOM_JOIN_SUCCESS, alreadyCode));
   } catch(error) {
     console.error(error);
-    return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.SIGN_UP_FAIL));
+    return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.ROOM_JOIN_FAIL));
   }
 })
 
